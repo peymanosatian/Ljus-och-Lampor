5e930f3fc1103ccf41d5646efcf1133b12a6c34b
Peyman <peyman.osatian@gmail.com>
2024-07-07 19:43:55 +0200

    footer

---
+++ Ljus och Lampor/View/Home/Footer.swift
@@ -0,0 +1,18 @@
+//
+//  Footer.swift
+//  Ljus och Lampor
+//
+//  Created by Peyman Osatian on 2024-07-07.
+//
+
+import SwiftUI
+
+struct Footer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Footer()
+}
