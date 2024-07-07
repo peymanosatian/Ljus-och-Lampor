d86686b0fae18ec8cf20039deb3e45d2c48b587a
Peyman <peyman.osatian@gmail.com>
2024-07-07 23:07:27 +0200

    Header

---
+++ Ljus och Lampor/View/Home/Header.swift
@@ -0,0 +1,18 @@
+//
+//  Header.swift
+//  Ljus och Lampor
+//
+//  Created by Peyman Osatian on 2024-07-07.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Header()
+}
---
+++ Ljus och Lampor/View/Home/Logo.swift
@@ -0,0 +1,18 @@
+//
+//  Logo.swift
+//  Ljus och Lampor
+//
+//  Created by Peyman Osatian on 2024-07-07.
+//
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Logo()
+}
