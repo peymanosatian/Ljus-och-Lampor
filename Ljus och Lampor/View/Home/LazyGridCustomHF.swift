09f200c963d760f915c6dd98becbcbc0b4add76b
Peyman <peyman.osatian@gmail.com>
2024-07-13 02:25:20 +0200

    final lazygrid

---
+++ Ljus och Lampor/View/Home/LazyGridCustomHF.swift
@@ -0,0 +1,18 @@
+//
+//  LazyGridCustomHF.swift
+//  Ljus och Lampor
+//
+//  Created by Peyman Osatian on 2024-07-13.
+//
+
+import SwiftUI
+
+struct LazyGridCustomHF: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyGridCustomHF()
+}
