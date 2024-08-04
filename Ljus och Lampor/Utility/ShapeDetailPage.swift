568ad654b426d683292d1b6f92e904d280304fa2
Peyman <peyman.osatian@gmail.com>
2024-08-04 19:31:28 +0200

    detailviewpush2

---
+++ Ljus och Lampor/Utility/ShapeDetailPage.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeDetailPage.swift
+//  Ljus och Lampor
+//
+//  Created by Peyman Osatian on 2024-08-04.
+//
+
+import SwiftUI
+
+struct ShapeDetailPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapeDetailPage()
+}
