ead4b7b84a2eeb3bd617df50c0c3fe4d0a5af845
Peyman <peyman.osatian@gmail.com>
2024-08-05 20:53:09 +0200

    rating , color

---
+++ Ljus och Lampor/View/Detail/Rate.swift
@@ -0,0 +1,18 @@
+//
+//  Rate.swift
+//  Ljus och Lampor
+//
+//  Created by Peyman Osatian on 2024-08-05.
+//
+
+import SwiftUI
+
+struct Rate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Rate()
+}
