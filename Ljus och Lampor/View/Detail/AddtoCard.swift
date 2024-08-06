6ef73e802f0a61ba6e5bf979f10d189e6f79bed0
Peyman <peyman.osatian@gmail.com>
2024-08-06 21:36:22 +0200

    detailviewdone

---
+++ Ljus och Lampor/View/Detail/AddtoCard.swift
@@ -0,0 +1,18 @@
+//
+//  AddtoCard.swift
+//  Ljus och Lampor
+//
+//  Created by Peyman Osatian on 2024-08-06.
+//
+
+import SwiftUI
+
+struct AddtoCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddtoCard()
+}
